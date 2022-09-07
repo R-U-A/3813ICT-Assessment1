7b9bbc304d879d2a9538f354f7ace908994b5854
R-U-A <richard.budden@outlook.com>
2022-09-07 22:33:56 +1000

    add user component added, with html and navigation.

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountComponent } from './account/account.component';
+import { AddUserComponent } from './add-user/add-user.component';
 import { ChatboxComponent } from './chatbox/chatbox.component';
 import { AuthGuard } from './guards/auth.guard';
 import { HomeComponent } from './home/home.component';
@@ -10,7 +11,8 @@ const routes: Routes = [
   {path: 'chatbox', component: ChatboxComponent, canActivate: [AuthGuard]},
   {path: 'account', component: AccountComponent, canActivate: [AuthGuard]},
   {path: '', component: LoginComponent},
-  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]}
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+  {path: 'adduser', component: AddUserComponent, canActivate:[AuthGuard]}
 ];
 
 @NgModule({
@@ -18,4 +20,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [AccountComponent, ChatboxComponent, LoginComponent, HomeComponent] 
+export const routingComponents = [AccountComponent, ChatboxComponent, LoginComponent, HomeComponent, AddUserComponent] 
--- my-app/src/app/service/authentication/authentication.service.ts
+++ my-app/src/app/service/authentication/authentication.service.ts
@@ -1,52 +1,55 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { user } from 'src/app/interfaces/user';
 import { SignInData } from 'src/app/model/signInData';
 import usersData from '../../data/user.json';
-//import user from '../../interfaces/user'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  private readonly adminUser = new SignInData('john_smith@gmail.com', 'abc123');
   isAuthenticated = false;
   isAdmin = false; 
 
   constructor(private router: Router) { }
 
   authenticate(signInData: SignInData): boolean {
-    if(this.checkCredentials(signInData)){
-      this.isAuthenticated = true;
-      if (typeof(Storage)!== "undefined"){
-        localStorage.setItem("Current User", JSON.stringify(signInData));
+    for (let i = 0; i < usersData.length; i++)
+    { console.log(usersData[i])
+      if(this.checkCredentials(signInData, usersData[i])){
+        this.isAuthenticated = true;
+        if (typeof(Storage)!== "undefined"){
+          localStorage.setItem("Current User", JSON.stringify(signInData));
+        }
+        else{
+          console.log("No storage Support");
+        }
+        if (usersData[i].userAdmin){
+          this.isAdmin = true;
+        }
+        else{
+          this.isAdmin = false; 
+        }
+        this.router.navigate(['home']); 
+        return true; 
       }
-      else{
-        console.log("No storage Support");
-      }
-      this.router.navigate(['home']); 
-      return true; 
     }
     this.isAuthenticated = false;
     this.isAdmin = false;
     return false;
   }
 
-  //private checkAdmin(user: User): boolean {
-  //  return this.userAdmin;
-  //}
-
-  private checkCredentials(signInData: SignInData): boolean {
-    return this.checkEmail(signInData.getEmail()) && this.checkPassword(signInData.getPassword()); 
+  private checkCredentials(signInData: SignInData, User: user): boolean {
+    return this.checkEmail(signInData.getEmail(), User.userEmail) && this.checkPassword(signInData.getPassword(), User.userPassword); 
   }
 
-  private checkEmail(email: string): boolean {
-    return email === this.adminUser.getEmail();
+  private checkEmail(email: string, user_list_email: string): boolean {
+    return email === user_list_email;
   }
 
-  private checkPassword(password: string): boolean {
-    return password === this.adminUser.getPassword();
-
+  private checkPassword(password: string, user_list_password: string): boolean {
+    return password === user_list_password;
   }
 
   logout(){
